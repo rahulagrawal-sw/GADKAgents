3a81f6d8f69ae68880588043f3be8666255ad5c1
Rahul Agrawal <rahulagrawal.sw@gmail.com>
2026-04-01 14:38:03 +0530

    ADK Agent with Custom Tools using stdio transport

---
+++ agent_with_tools/agent.py
@@ -0,0 +1,77 @@
+import asyncio
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+from google.adk.agents.llm_agent import Agent
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+from google.adk.tools.mcp_tool import MCPToolset, StdioConnectionParams
+
+from google.genai import types
+
+APP_NAME = "agent_with_runner_app"
+USER_ID = "rahul_12345"
+SESSION_ID = "rahul_12345"
+
+async def get_agent():
+
+    # create a toolset
+    toolset = MCPToolset(
+        connection_params=StdioConnectionParams(
+            server_params={
+                "command": "python",
+                "args": ["mcpserver.py"]
+            }
+        )
+    )
+
+    root_agent = Agent(
+        model='gemini-2.5-flash',
+        name='root_agent_2',
+        description='Google ADK Agent with Runner & Tools - get_weather',
+        instruction='You are a helpful assistant',
+        tools=[toolset]
+    )
+
+    return root_agent, toolset
+
+async def main(user_query):
+    # create a memory session
+    session_service = InMemorySessionService()
+    await session_service.create_session(app_name= APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
+
+    # get root agent
+    my_agent, toolset = await get_agent()
+
+    # create a runner
+    runner = Runner(
+        app_name=APP_NAME,
+        agent=my_agent,
+        session_service=session_service
+    )
+    
+     # format the query 
+    user_content = types.Content(role = "user", parts= [types.Part(text=user_query)])
+    print("Running agent with query:", user_query)
+    
+    # run the runner
+    events = runner.run_async(
+            user_id=USER_ID,
+            session_id=SESSION_ID,
+            new_message=user_content
+        )
+    
+    # print the response
+    async for event in events:
+        if event.is_final_response():
+            final_response = event.content.parts[0].text
+            print("Agent Response:", final_response)
+
+    # close MCP Server
+    await toolset.close()
+
+
+if __name__ == "__main__":
+    user_query = "How is weather in Delhi?"
+    asyncio.run(main(user_query))
