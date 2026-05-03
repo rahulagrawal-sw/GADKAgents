0e474c7ce962e8975781b86678121254693a91ff
Rahul Agrawal <rahulagrawal.sw@gmail.com>
2026-05-03 16:11:36 +0530

    My LLM Agent with runner

--- _2_agent_with_runner/agent.py
+++ _2_agent_with_runner/agent.py
@@ -1,61 +1,63 @@
 import asyncio
 
-from dotenv import load_dotenv
-load_dotenv(override=True)
-
 from google.adk.agents.llm_agent import Agent
-from google.adk.sessions import InMemorySessionService
 from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
 
 from google.genai import types
 
-APP_NAME = "agent_with_runner_app"
-USER_ID = "rahul_12345"
-SESSION_ID = "rahul_12345"
+from dotenv import load_dotenv
 
-async def get_agent():
-    root_agent = Agent(
-        model='gemini-2.5-flash',
-        name='root_agent_2',
-        description='Google ADK Agent with Runner',
-        instruction='You are a helpful assistant',
+load_dotenv(override=True)
+
+APP_NAME = "My_LLM_Agent_App"
+USER_ID = "rahul"
+SESSION_ID = "12345"
+
+def get_agent() -> Agent:
+    my_llm_agent = Agent(
+        name = "MyLLMAgent",
+        description="My LLM Agent for runner example",
+        model="gemini-2.5-flash",
+        instruction="You are a helpful assistant. Give clear and concise answers"
     )
+    return my_llm_agent
 
-    return root_agent
 
-async def main(user_query):
-    # create a memory session
+async def run_agent(user_input_query: str) -> str:
+    output_result = ""
+    # STEP - 1: Create InMemory SessionService
     session_service = InMemorySessionService()
-    await session_service.create_session(app_name= APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
+    await session_service.create_session(app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
 
-    # get root agent
-    my_agent = await get_agent()
+    # STEP - 2: ADK Agent
+    my_llm_agent = get_agent()
 
-    # create a runner
+    # STEP - 3: Runner
     runner = Runner(
-        app_name=APP_NAME,
-        agent=my_agent,
-        session_service=session_service
+        app_name = APP_NAME,
+        agent = my_llm_agent,
+        session_service = session_service,
     )
-    
-     # format the query 
-    user_content = types.Content(role = "user", parts= [types.Part(text=user_query)])
-    print("Running agent with query:", user_query)
-    
-    # run the runner
-    events = runner.run_async(
-            user_id=USER_ID,
-            session_id=SESSION_ID,
-            new_message=user_content
-        )
-    
-    # print the response
+
+    # STEP - 4: Prepare user query content
+    user_msg = types.Content(role = "user", parts= [types.Part(text=user_input_query)])
+
+    # STEP - 5: Run the runner
+    events = runner.run_async(user_id=USER_ID, session_id=SESSION_ID, new_message=user_msg)
+
+    # STEP - 6: Print the response
     async for event in events:
         if event.is_final_response():
             final_response = event.content.parts[0].text
-            print("Agent Response:", final_response)
+            print(f"final_response: {final_response}")
+            output_result = final_response
+    
+    print(f"output_result: {output_result}")
+    return output_result
 
 
 if __name__ == "__main__":
-    user_query = "Why use google adk?"
-    asyncio.run(main(user_query))
+    user_input_query = "2+5"
+    print(f"user_input_query: {user_input_query}")
+    asyncio.run(run_agent(user_input_query))
---
+++ _2_agent_with_runner/old_agent.py
@@ -0,0 +1,61 @@
+import asyncio
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+from google.adk.agents.llm_agent import Agent
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+
+from google.genai import types
+
+APP_NAME = "agent_with_runner_app"
+USER_ID = "rahul_12345"
+SESSION_ID = "rahul_12345"
+
+async def get_agent():
+    root_agent = Agent(
+        model='gemini-2.5-flash',
+        name='root_agent_2',
+        description='Google ADK Agent with Runner',
+        instruction='You are a helpful assistant',
+    )
+
+    return root_agent
+
+async def main(user_query):
+    # create a memory session
+    session_service = InMemorySessionService()
+    await session_service.create_session(app_name= APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
+
+    # get root agent
+    my_agent = await get_agent()
+
+    # create a runner
+    runner = Runner(
+        app_name=APP_NAME,
+        agent=my_agent,
+        session_service=session_service
+    )
+    
+     # format the query 
+    user_content = types.Content(role = "user", parts= [types.Part(text=user_query)])
+    print("Running agent with query:", user_query)
+    
+    # run the runner
+    events = runner.run_async(
+            user_id=USER_ID,
+            session_id=SESSION_ID,
+            new_message=user_content
+        )
+    
+    # print the response
+    async for event in events:
+        if event.is_final_response():
+            final_response = event.content.parts[0].text
+            print("Agent Response:", final_response)
+
+
+if __name__ == "__main__":
+    user_query = "Why use google adk?"
+    asyncio.run(main(user_query))
